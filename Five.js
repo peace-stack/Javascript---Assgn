109fcafd9879e8bf9bc066d0e3d2a55683c9ab8c
peace-stack <60148834+peace-stack@users.noreply.github.com>
2023-02-14 11:41:02 +0100

    Changes to my number 5 question

--- Five.js
+++ Five.js
@@ -1,9 +1,23 @@
-let details ={ 
-    myName :"Peace Agboola",
-    myGender :"Female",
-    myCourse :"Agricultural Economics",
-    myInstituion :"Obafemi Awolowo university",
-};
-// console.log ( details.myName + ('') + details.myGender + ('') + details.myCourse + ('') + details.myInstituion);
+let details = new function(){ 
+    this.myName ="Peace Agboola";
+    this.myGender = "Female",
+    myCourse = "Agricultural Economics",
+    myInstituion ="Obafemi Awolowo university",
+}
+console.log ( details.myName + ('') + details.myGender + ('') + details.myCourse + ('') + details.myInstituion);
 
-console.log(details);
+function Calc (Arithmetic) {
+    this.firstValue = 10
+    this.secondValue = 7
+    this.thirdValue = 19
+    this.firstFunc = function dFirstCalc (firstValue,secondValue, thirdValue){
+         firstFunc = ((this.firstValue + this.secondValu)- this.thirdValue)
+        dFirstCalc( 10 , 7, 19)
+    }
+}
+    
+    this.secondFunc = function dSecondCalc (){
+        this.secondFunc = ((this.secondValue * this.thirdValue) + this.firstValue)
+        dSecondCalc(19 + 7 + 10);
+    }
+    
