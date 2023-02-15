c6a67bbb460875660b57caa7d91e3660fca721dc
peace-stack <60148834+peace-stack@users.noreply.github.com>
2023-02-15 21:02:05 +0100

    made some changes to my solution

--- Five.js
+++ Five.js
@@ -1,56 +1,40 @@
-let details = new Object()         
-    details.myName ="Peace Agboola";
-    details.myGender = "Female",
-   details.myCourse = "Agricultural Economics",
-   details.myInstituion ="Obafemi Awolowo university",
-console.log ( details.myName + ' ' + details.myGender + ' ' + details.myCourse + ' ' + details.myInstituion);
+let details = new Object();
+details.myName = "Peace Agboola";
+(details.myGender = "Female"),
+  (details.myCourse = "Agricultural Economics"),
+  (details.myInstituion = "Obafemi Awolowo university"),
+  console.log(
+    details.myName +
+      " " +
+      details.myGender +
+      " " +
+      details.myCourse +
+      " " +
+      details.myInstituion
+  );
 
+let Calc = {
+  firstValue: 10,
+  secondValue: 7,
+  thirdValue: 19,
+  firstFunc() {
+    let math = this.firstValue + this.secondValue - this.thirdValue;
+    return math;
+  },
 
-    let Calc = {
-    firstValue: 10,
-    secondValue :7,
-    thirdValue :19,
-    firstFunc (){
-        let math= ((this.firstValue + this.secondValue)- this.thirdValue)
-        return math
-    },
+  secondFunc() {
+    let mathTwo = this.secondValue * this.thirdValue + this.firstValue;
+    return mathTwo;
+  },
+};
+console.log(Calc.firstFunc());
+console.log(Calc.secondFunc());
 
-    secondFunc (){
-        let mathTwo = ((this.secondValue * this.thirdValue) + this.firstValue)
-        return mathTwo
-    }
-}
- console.log (Calc.firstFunc())
- console.log (Calc.secondFunc())
+let obj = new Object();
+obj.name = "Peace Agboola";
+obj.gender = "Female";
+obj.course = "Agricultural Economics";
+obj.institution = "OAU";
+let objSentence = `My name is ${obj.name} I am a ${obj.gender} i studied ${obj.course} from ${obj.institution}`;
 
-
-
-
-let obj= new Object() 
-obj.name = "Peace Agboola"
-obj.gender = "Female"
-obj.course = "Agricultural Economics"
-obj.institution = "OAU"
-let objSentence = `My name is ${obj.name} I am a ${obj.gender} i studied ${obj.course} from ${obj.institution}`
-
-console.log(objSentence)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+console.log(objSentence);
